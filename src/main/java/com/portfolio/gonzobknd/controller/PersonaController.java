0388f3fd6f593f28712cf562be957c9f39dde50b
g0nnzo <bonzai_rock@hotmail.com>
2022-08-31 02:43:38 -0300

    cambié los endpoints locales de el front en los controllers del back por los endpoints de firebase

--- src/main/java/com/portfolio/gonzobknd/controller/PersonaController.java
+++ src/main/java/com/portfolio/gonzobknd/controller/PersonaController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://gonzoportfoliofront.web.app")
 public class PersonaController {
 
     @Autowired
